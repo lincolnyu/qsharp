112ba4f89a87824c531a6b9f37e15a7e84d356c8
quanb <quanb@QUANBEN-ASUS>
2017-04-09 14:30:34 +1000

    Added cache and LRU policy to Scheme.Utility.

---
+++ QSharp/QSharp.Scheme.Utility/Cache.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace QSharp.Scheme.Utility
+{
+    public abstract class Cache<TKey, TValue>
+    {
+        public delegate TValue GetValueCb(TKey key);
+        public delegate void UpdateCacheCb(IDictionary<TKey, TValue> dict, TKey key, TValue val);
+
+        private GetValueCb _getValueCb;
+        private UpdateCacheCb _updateCacheCb;
+        private Dictionary<TKey, TValue> _dict = new Dictionary<TKey, TValue>();
+
+        public Cache(GetValueCb getValueCb, UpdateCacheCb updateCb)
+        {
+            _getValueCb = getValueCb;
+            _updateCacheCb = updateCb;
+        }
+
+        public TValue Get(TKey key)
+        {
+            TValue val;
+            if (!_dict.TryGetValue(key, out val))
+            {
+                val = _getValueCb(key);
+                _updateCacheCb(_dict, key, val);
+            }
+            return val;
+        }
+
+    }
+}
---
+++ QSharp/QSharp.Scheme.Utility/LruCachePolicy.cs
@@ -0,0 +1,56 @@
+﻿using QSharp.Scheme.Classical.Trees;
+using System.Collections.Generic;
+
+namespace QSharp.Scheme.Utility
+{
+    public class LruCachePolicy<TKey, TValue>
+    {
+        class KeyVisit
+        {
+            public TKey Key;
+            public ulong VisitCount;
+        }
+
+        private Dictionary<TKey, KeyVisit> _visitDict = new Dictionary<TKey, KeyVisit>();
+
+        private AvlTree<KeyVisit> _visitTree = new AvlTree<KeyVisit>((a,b)=>a.VisitCount.CompareTo(b.VisitCount));
+
+        public ulong _nextCount = 0;
+
+        LruCachePolicy(uint cacheSize)
+        {
+            CacheSize = cacheSize;
+        }
+
+        public uint CacheSize { get; }
+        
+        public void UpdateCache(IDictionary<TKey, TValue> dict, TKey key, TValue val)
+        {
+            var visit = new KeyVisit { Key = key, VisitCount = _nextCount };
+            _visitDict[key] = visit;
+            _visitTree.Insert(visit);
+            dict[key] = val;
+            while (_visitDict.Count > CacheSize)
+            {
+                var oldest = GetOldestVisit();
+                var keyToRemove = oldest.Entry.Key;
+                _visitDict.Remove(key);
+                dict.Remove(key);
+                _visitTree.Remove(oldest);
+            }
+        }
+
+        private AvlTreeWorker.INode<KeyVisit> GetOldestVisit()
+        {
+            var v = _visitTree.Root;
+            while (true)
+            {
+                if (v.Left == null)
+                {
+                    return (AvlTreeWorker.INode<KeyVisit>)v;
+                }
+                v = v.Left;
+            }
+        }
+    }
+}
