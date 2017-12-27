b70f6de8c130a31f3ee53f40903173857174d8e5
Lincoln Yu <quanben@msn.com>
2017-12-28 01:41:00 +1100

    Add Condition2 class and DotNetCore test project stub.

---
+++ QSharp/QSharp.Scheme.Threading/Condition2.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading;
+
+namespace QSharp.Scheme.Threading
+{
+    // TODO test this...
+    /// <summary>
+    ///  Condition class that is supposed to offer similar functionality to boost's condition class
+    /// </summary>
+    public class Condition2
+    {
+        public delegate bool Predicate();
+        public delegate void Action();
+
+        public void WaitUntil(Predicate predicate, Action preWait, Action postWait)
+        {
+            var lockWasTaken = false;
+            try
+            {
+                Monitor.Enter(this, ref lockWasTaken);
+                preWait();
+                while (!predicate())
+                {
+                    Monitor.Wait(this);
+                }
+                postWait();
+            }
+            finally
+            {
+                if (lockWasTaken)
+                {
+                    Monitor.Exit(this);
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Action on the condition variable and notify threads waiting
+        /// </summary>
+        /// <param name="action">
+        ///  Actions on the variable and calls either Monitor.PulseAll(this) 
+        ///  or Monitor.Pulse(this) afterwards
+        /// </param>
+         
+        public void ActionAndNotify(Action action)
+        {
+            var lockWasTaken = false;
+            try
+            {
+                Monitor.Enter(this, ref lockWasTaken);
+                action();
+            }
+            finally
+            {
+                if (lockWasTaken)
+                {
+                    Monitor.Exit(this);
+                }
+            }
+        }
+    }
+}
